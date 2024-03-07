c8dc75e05f8ca7c912b58ce673d19bf005411539
anhbanlinhle <vietlinhleviet@gmail.com>
2024-03-08 00:38:00 +0700

    feat: render table and chart from dummy data

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,7 +1,62 @@
 import { json } from 'body-parser';
 
 let home = async (req, res) => {
-  return res.render('home.ejs')
+  let processes = {
+    processes: [
+    {
+      name: 'P1',
+      arrive: 0,
+      burst: 0,
+      priority: 0,
+      responseTime: 0,
+      turnaroundTime: 0,
+      waitTime: 0,
+    },
+    {
+      name: 'P2',
+      arrive: 0,
+      burst: 0,
+      priority: 0,
+      responseTime: 0,
+      turnaroundTime: 0,
+      waitTime: 0,
+    },
+    {
+      name: 'P3',
+      arrive: 0,
+      burst: 0,
+      priority: 0,
+      responseTime: 0,
+      turnaroundTime: 0,
+      waitTime: 0,
+    },
+    {
+      name: 'P4',
+      arrive: 0,
+      burst: 0,
+      priority: 0,
+      responseTime: 0,
+      turnaroundTime: 0,
+      waitTime: 0,
+    },
+    {
+      name: 'P5',
+      arrive: 0,
+      burst: 0,
+      priority: 0,
+      responseTime: 0,
+      turnaroundTime: 0,
+      waitTime: 0,
+    }
+    ],
+    ganttChart: [
+      'P1', 'P2', 'P3', 'P4', 'P5'
+    ],
+    averageResponseTime: 0.0,
+    averageTurnaroundTime: 0.0,
+    averageWaitTime: 0.0
+  }
+  return res.render('home.ejs', {processes: processes, gantt_chart: processes.ganttChart})
 }
 
 module.exports = {
